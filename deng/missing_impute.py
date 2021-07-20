30ee9f5fd6a124827daa1110098434925aaae915
myloansk <cosmyl77@gmail.com>
2021-07-20 07:27:04 +0300

    Refactor handle method of AbstractNullHandler NullHanldler classes and add List from typing pkg

--- deng/missing_impute.py
+++ deng/missing_impute.py
@@ -1,57 +1,48 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Any, Optional
+from typing import Any, Optional,List
 
 class NullHandler(ABC):
-
 
     @abstractmethod
     def set_next(self,null_handler:NullHandler) -> NullHandler:
         pass
     @abstractmethod
-    def handle(self,request)->Optional:
+    def handle(self,impute_method,sdf,colList):
         pass
-
-
 class AbstractNullHandler(NullHandler):
 
 
     _next_handler: NullHandler = None
 
     def set_next(self,null_handler:NullHandler) -> NullHandler:
-
         self._next_handler = null_handler
-
         return null_handler
 
     @abstractmethod
-    def handle(self,request: Any) -> str:
-        if(self._next_handler):
-            return self._next_handler.handle(request)
-
-        return asNondeterministic
+    def handle(self,impute_method,sdf,colList):
+        if self._next_handler:
+            return self._next_handler.handle(impute_method,sdf,colList)
+        return sdf
 
 
 class ZeroImputeHandler(AbstractNullHandler):
-    def handle(self,request:Any) -> str:
+    def handle(self,impute_method,sdf,colList):
         pass
-
 class MeanImputeHandler(AbstractNullHandler):
-    def handle(self,request:Any) -> str:
+    def handle(self,impute_method,sdf,colList):
         pass
 
 class UnknownImputeHandler(AbstractNullHandler):
-    def handle(self,request:Any) -> str:
+    def handle(self,impute_method,sdf,colList):
         pass
-
 class FfillImputeHandler(AbstractNullHandler):
-    def handle(self,request:Any) -> str:
+    def handle(self,impute_method,sdf,colList):
         pass
 
 class NfillImputeHandler(AbstractNullHandler):
-    def handle(self,request:Any) -> str:
+    def handle(self,impute_method,sdf,colList):
         pass
-
 class BucketImputeHandler(AbstractNullHandler):
-    def handle(self,request:Any) -> str:
+    def handle(self,impute_method,sdf,colList):
         pass
