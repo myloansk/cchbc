cf0ff5da62d0d4a2896875e74571ab747c40a949
myloansk <cosmyl77@gmail.com>
2021-07-20 07:29:38 +0300

    Add handle method for MeanImputeHandler

--- deng/missing_impute.py
+++ deng/missing_impute.py
@@ -35,7 +35,37 @@ class ZeroImputeHandler(AbstractNullHandler):
                 return  super().handle(impute_method,sdf,colList)
 class MeanImputeHandler(AbstractNullHandler):
     def handle(self,impute_method,sdf,colList):
-        pass
+        if impute_method == 'avg':
+
+                w = Window().partitionBy('employee_id').orderBy(f.col('year_month').cast("integer").desc())
+                w1 = Window().partitionBy('employee_id').orderBy(f.col('year_month').cast("integer").desc()).rowsBetween(0,3)
+                w2 = Window().partitionBy('employee_id').orderBy(f.col('year_month').cast("integer").desc()).rowsBetween(0,6)
+                w3 = Window().orderBy(f.col('year_month').cast("integer").desc()).rowsBetween(0,3)
+                w4 = Window().orderBy(f.col('year_month').cast("integer").desc()).rowsBetween(0,6)
+                w5 = Window().orderBy(f.col('year_month').cast("integer").desc())
+                for colName in colList:
+                    sdf = sdf.withColumn('rolling_mean_per_bd_1Mon'+ colName,f.first(f.col(colName)).over(w))\
+                                               .withColumn('rolling_mean_per_bd_3Mon' + colName,f.mean(f.col(colName)).over(w1))\
+                                               .withColumn('rolling_mean_per_bd_6Mon' + colName,f.mean(f.col(colName)).over(w2))\
+                                               .withColumn('rolling_mean_total_3Mon' + colName,f.mean(f.col(colName)).over(w3))\
+                                               .withColumn('rolling_mean_total_6Mon' + colName,f.mean(f.col(colName)).over(w4))\
+                                               .withColumn('rolling_mean_total' + colName,f.mean(f.col(colName)).over(w5))\
+                                               .withColumn("rolling_mean_impute" + colName,
+                                                           f.coalesce(f.col("rolling_mean_per_bd_1Mon" + colName),
+                                                                       f.col("rolling_mean_per_bd_3Mon" + colName),
+                                                                       f.col("rolling_mean_per_bd_6Mon"+ colName),
+                                                                       f.col("rolling_mean_total_3Mon" + colName),
+                                                                       f.col("rolling_mean_total_6Mon" + colName),
+                                                                       f.col("rolling_mean_total" + colName)).cast("string"))
+
+
+                for colName in colList:
+                   sdf = sdf.withColumn(colName,
+                                        f.coalesce(f.col(colName),f.col("rolling_mean_impute" + colName)).cast('float'))
+
+                return sdf
+            else:
+                return super().handle(impute_method,sdf,colList)
 
 class UnknownImputeHandler(AbstractNullHandler):
     def handle(self,impute_method,sdf,colList):
