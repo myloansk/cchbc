5ab806a41925ce4667a501a7bc125d7b9bc20034
myloansk <cosmyl77@gmail.com>
2021-07-24 22:58:33 +0300

    Add class DataCheck that calc descriptive stats on the provided data

---
+++ deng/dataset_check.py
@@ -0,0 +1,30 @@
+class DataCheck():
+    """
+    class DataCheck encapsulates a number of methods that
+    calculates descriptive stats on the provided dataset
+
+    :param DataFrame sdf: pyspark DataFrame on which descriptive stats wiil be calculated
+    :attr sdf:
+
+    """
+    def __init__(sdf:DataFrame)->None:
+        """
+        """
+        self.Dframe = Dframe
+
+    def cnt_rows(self):
+        pass
+    def cnt_column(self):
+        pass
+
+    def data_types(self):pass
+        pass
+
+    def summary(self):pass
+
+    def diff_sdf(self):pass
+
+    def null_percentage_per_column(self):
+         pass
+    def show_five(self):
+        pass
