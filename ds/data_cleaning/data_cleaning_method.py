8b3a52c35423f54ad11a01b33d2a1df3c3f0ff22
myloansk <cosmyl77@gmail.com>
2021-07-30 11:15:49 +0300

    Add module skeleton

--- ds/data_cleaning/data_cleaning_method.py
+++ ds/data_cleaning/data_cleaning_method.py
@@ -14,8 +14,25 @@ from pyspark.ml.feature import (OneHotEncoder,
                                 ChiSqSelector,
                                 StringIndexer,
                                 VectorAssembler,
-                                BucketedRandomProjectionLSH,
                                 VectorSlicer)
 
 
 from pyspark.ml import Pipeline
+
+class DataCleaner():
+    """DataCleaner class """
+
+  def __init__(self,method:Method) -> None:
+      self._method = method
+ @property
+ def method(self) -> Method:
+
+    return self._method
+
+ @method.setter
+ def method(self,method:Method) ->None:
+     self._method = method
+
+
+
+ 
